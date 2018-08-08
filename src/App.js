3781aed300c141b236414b9949d5af0b4c99a2d5
Tyler Wasson <wasson@Tylers-MacBook-Pro.local>
2018-08-07 20:19:26 -0400

    Finished Auth with logout button and view change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
 import { View } from 'react-native';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 
 class App extends Component {
+    state= {
+        loggedIn: null
+    }
+
     componentWillMount() {
         firebase.initializeApp({
             apiKey: 'AIzaSyBkmMOzyXDG__2LFpNMCIkmSYJFo9ObDGs',
@@ -15,16 +19,52 @@ class App extends Component {
             storageBucket: 'auth-d8049.appspot.com',
             messagingSenderId: '452133512221'
         });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        });
+    }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                <View style={styles.buttonStyle}>
+                    <Button onPress={() => firebase.auth().signOut()}>
+                        Log Out
+                    </Button>
+                </View>
+                );
+            case false:
+                return <LoginForm />;
+            default:
+                return <View style={styles.spinnerStyle}><Spinner size='large' /></View>;
+        }
     }
 
     render() {
         return (
             <View>
             <Header headerText="Authentication" />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
     }
 }
+
+const styles = {
+    buttonStyle: {
+        flexDirection: 'row',
+        paddingTop: 10
+    },
+    spinnerStyle: {
+        flexDirection: 'row',
+        paddingTop: 100
+    }
+};
 
 export default App;
